01182d530b46f23325837d6d4eaccefb1b526d25
hp <hp@192.168.1.38>
2017-08-05 10:46:24 +0530

    first hw

---
+++ WebBhumi/src/HomeWork/MethodTest.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HomeWork;
+
+import Methods.*;
+
+/**
+ *
+ * @author web-bhumi
+ */
+public class MethodTest {
+    private String name;
+    private String address;
+    private int age;
+    private long number;
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        if(age>=18)
+        this.age = age;
+    }
+    public static void main(String[] args) {
+        MethodTest mt = new MethodTest();
+        mt.setName("Anjali");
+        mt.setAddress("Indore");
+        mt.setAge(22);
+        
+        System.out.println("Name- "+mt.getName()+" Address:- "+mt.getAddress()+" age:-"+mt.getAge());
+        mt.setNumber(9826055360);
+        System.out.println("Number- "+mt.getNumber()
+    }
+
+    /**
+     * @return the number
+     */
+    public long getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(long number) {
+        this.number = number;
+    }
+    
+}
+
